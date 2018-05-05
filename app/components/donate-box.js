fff24e8fb6266b0afd09d6bb0ac014e1b610c9f6
robabby <robabby23@gmail.com>
2018-05-05 15:14:13 -0500

    Service Time

--- app/components/donate-box.js
+++ app/components/donate-box.js
@@ -1,54 +1,28 @@
 import Component from '@ember/component';
-import { computed, get, set } from "@ember/object";
+import { alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  campaignService: service(),
   classNames: ['donate-box'],
   classNameBindings: ['isComplete:is-complete'],
-  campaignGoal: 5000,
-  donation: 5,
-  donationTotal: 0,
-  totalDonors: 0,
-  errorMessage: '',
-  buttonText: computed('remainingAmount', function() {
-    return this.get('isComplete') ? 'All Done' : 'Give Now';
-  }),
-  isComplete: computed('remainingAmount', function() {
-    return this.get('remainingAmount') === 0 ? true : false;
-  }),
-  remainingAmount: computed('donationTotal', function() {
-    let remainingAmount = get(this, 'campaignGoal') - get(this, 'donationTotal');
-    let displayTotal;
-    if (remainingAmount <= 0) {
-      displayTotal = 0;
-    } else {
-      displayTotal = remainingAmount;
-    }
-    return displayTotal;
-  }),
-  campaignPercent: computed('donationTotal', function() {
-    return Math.ceil(get(this, 'donationTotal') / get(this, 'campaignGoal') * 100);
-  }),
+
+  campaignGoal: alias('campaignService.campaignGoal'),
+  donation: alias('campaignService.donation'),
+  totalDonations: alias('campaignService.totalDonations'),
+  totalDonors: alias('campaignService.totalDonors'),
+  errorMessage: alias('campaignService.errorMessage'),
+  buttonText: alias('campaignService.buttonText'),
+  isComplete: alias('campaignService.isComplete'),
+  remainingAmount: alias('campaignService.remainingAmount'),
+  campaignPercent: alias('campaignService.campaignPercent'),
+
   didRender() {
     this.$('.donate-box__input').focus();
   },
   actions: {
-    addDonation(donation) {
-      let newDonation = parseInt(donation);
-
-      if (newDonation === 0) {
-        set(this, 'errorMessage', 'You need to enter an amount!');
-      } else if (newDonation % 5 === 0) {
-        let donationTotal = get(this, 'donationTotal');
-        let newDonor = get(this, 'totalDonors');
-        let newTotal = donationTotal + newDonation;
-        set(this, 'donationTotal', newTotal);
-        set(this, 'totalDonors', newDonor + 1);
-        set(this, 'donation', 5);
-        set(this, 'errorMessage', '');
-      } else {
-        set(this, 'errorMessage', 'Donations must be in increments of $5');
-        set(this, 'donation', 5);
-      }
+    newDonation(donation) {
+      this.get('campaignService').addDonation(donation);
     }
   }
 });
---
+++ app/services/campaign-service.js
@@ -0,0 +1,47 @@
+import Service from '@ember/service';
+import { computed, get, set } from "@ember/object";
+
+export default Service.extend({
+  campaignGoal: 5000,
+  donation: 5,
+  totalDonations: 0,
+  totalDonors: 0,
+  errorMessage: '',
+  buttonText: computed('remainingAmount', function() {
+    return this.get('isComplete') ? 'All Done' : 'Give Now';
+  }),
+  isComplete: computed('remainingAmount', function() {
+    return this.get('remainingAmount') === 0 ? true : false;
+  }),
+  remainingAmount: computed('totalDonations', function() {
+    let remainingAmount = get(this, 'campaignGoal') - get(this, 'totalDonations');
+    let displayTotal;
+    if (remainingAmount <= 0) {
+      displayTotal = 0;
+    } else {
+      displayTotal = remainingAmount;
+    }
+    return displayTotal;
+  }),
+  campaignPercent: computed('totalDonations', function() {
+    return Math.ceil(get(this, 'totalDonations') / get(this, 'campaignGoal') * 100);
+  }),
+  addDonation(donation) {
+    let newDonation = parseInt(donation);
+
+    if (newDonation === 0) {
+      set(this, 'errorMessage', 'You need to enter an amount!');
+    } else if (newDonation % 5 === 0) {
+      let totalDonations = get(this, 'totalDonations');
+      let newDonor = get(this, 'totalDonors');
+      let newTotal = totalDonations + newDonation;
+      set(this, 'totalDonations', newTotal);
+      set(this, 'totalDonors', newDonor + 1);
+      set(this, 'donation', 5);
+      set(this, 'errorMessage', '');
+    } else {
+      set(this, 'errorMessage', 'Donations must be in increments of $5');
+      set(this, 'donation', 5);
+    }
+  }
+});
